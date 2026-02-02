ae59c9b63942694f7f6bd236f94d93869c08a511
Rishabh Rawat <137486565+kaizenrishi@users.noreply.github.com>
2026-02-02 13:56:49 +0530

    Implement rate limiting for API requests
    Added rate limiting to the Express app to restrict requests.

--- app.js
+++ app.js
@@ -2,10 +2,18 @@ import express from "express";
 import { body, validationResult } from "express-validator";
 import authRouter from "./src/routes/auth.routes.js";
 import urlRouter from "./src/routes/url.routes.js";
+import rateLimit from "express-rate-limit";
 
 export const app = express();
+// 10 req in 1 minutes
+const limiter = rateLimit({
+  windowMs: 1000 * 60 * 1,
+  max: 10,
+  message: "Too many request limit reached",
+});
 
 app.use(express.json());
+app.use(limiter);
 
 app.use("/api/auth", authRouter);
 
